0880cc2319610d7f17de5759267452c13d5bacba
DDonghyeo <gsu9045000@naver.com>
2022-09-19 00:24:38 +0900

    api

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,15 +18,50 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+    /*
+    문제점
+    1.엔티티의 모든 값이 노출됨 ->@JsonIgnore로 막을 수 있긴 함
+    2. 엔티티가 변경되면 api스펙이 변경됨
+    */
+
+
     @PostMapping("api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
-    //문제점
-    //1. 파라미터 뭐가 넘어올지 모름
-    //2. 멤버 엔티티를 그대로 이용함
+    /*문제점
+    1. 파라미터 뭐가 넘어올지 모름
+    2. 멤버 엔티티를 그대로 이용함
+    */
 
+    @GetMapping("api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
@@ -40,7 +77,8 @@ public class MemberApiController {
     public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
                                                @RequestBody @Valid UpdateMemberRequest request) {
         memberService.update(id, request.getName());
-
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
     @Data
@@ -65,8 +103,8 @@ public class MemberApiController {
     static class CreateMemberResponse{
         private Long id;
 
-        public CreateMemberResponse(Long id) {
-            this.id = id;
+        public CreateMemberResponse(Long id){
+            this.id=id;
         }
     }
 }
